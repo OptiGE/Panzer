5c7643411c0c752f55dcae39f88e61633636cce6
OptiGE <optige@gmail.com>
2019-06-15 19:07:13 +0100

    Fungerande knappar

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,8 @@
 var socket;
 var btn_joinroom;
+var canvasHeight;
+var canvasWidth;
+
 
 function preload() {
   //Ladda in non-sprite assets
@@ -8,19 +11,24 @@ function preload() {
 
 function setup() {
 	
+	//Sätt canvas width och height till något smart här. KOlla om man kan låsa rotation
+	
 	//Sapa canvas och sätt in bakgrund
 	createCanvas(windowWidth, windowHeight);
 	image(bg, 0, 0, windowWidth, windowHeight);
 	
-	//Skapa knapp-sprite och animation
-	btn_joinroom = createSprite((windowWidth - 600) / 2, 350);
-	btn_joinroom.addAnimation('standard', 'assets/btn_joinroom_up.png', 'assets/btn_joinroom_hv.png', 'assets/btn_joinroom_p.png');
-	//btn_joinroom.animation.playing = false;
-	btn_joinroom.onMouseOver = function() {
-		this.goToFrame(2);
-	};
-	btn_joinroom.playing = false;
-	 
+	//Sätt storlek för knappar
+	var buttonWidth = windowWidth / 12;
+	var buttonHeight = windowHeight / 4;
+	
+	
+	//Skapa Join room knapp
+	btn_joinroom = createSprite(0, 0, 600, 200);
+	addStdButton(btn_joinroom, 0, 'assets/btn_joinroom_up.png', 'assets/btn_joinroom_p.png');
+	
+	//Skapa Create room knapp
+	btn_createroom = createSprite(0, 0, 600, 200);
+	addStdButton(btn_createroom, 1, 'assets/btn_createroom_up.png', 'assets/btn_createroom_p.png');
 	
 	//Anslut till socketservern
 	socket = io.connect('http://192.168.0.29:3000');
@@ -29,6 +37,20 @@ function setup() {
 	
 	// För att förhindra scroll på mobilen
 	$('body').addClass('overflow'); 
+	
+	
+	
+	//Knapp-event hanterare
+	btn_joinroom.onMousePressed = function() {
+		console.log("btn_joinroom pressed");
+		btn_joinroom.animation.changeFrame(1);
+	};
+	
+	btn_createroom.onMousePressed = function() {
+		console.log("btn_createroom pressed");
+		btn_createroom.animation.changeFrame(1);
+	};
+	
 }
 
 function draw() {
@@ -46,13 +68,13 @@ function titlescreen() {
 
 // ------------------- M O B I L A N P A S S N I N G -----------------------------
 
-function touchStarted () {
+/* function touchStarted () {
   var fs = fullscreen();
   if (!fs) {
     fullscreen(true);
   }
 }
-
+ */
 /*
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -67,6 +89,15 @@ function windowResized() {
 
 
 
+function addStdButton(btn, heightOffset, img1, img2){
+	btn.addAnimation('standard', img1, img2);
+	btn.animation.playing = false;
+	btn.scale = windowHeight / (7*200); //scale sätts så att knappens höjd om 200 översätts till 1/7 av skärmen
+	btn.mouseActive = true;
+	btn.position.x = ((windowWidth) / 2);
+	btn.position.y = ((windowHeight + 150) / 2) + ((heightOffset * 200*btn.scale)*1.3);
+	console.log(btn.height);
+}
 
 
 
@@ -85,6 +116,11 @@ function windowResized() {
 
 
 
+function mouseReleased() {
+	console.log("Mouse released");
+	btn_joinroom.animation.changeFrame(0);
+	btn_createroom.animation.changeFrame(0);
+}
 
 
 
@@ -96,9 +132,7 @@ function windowResized() {
 
 
 
-
-
-function mouseDragged() {
+function mouse0Dragged() {
 	
 	var data = {
 		x: mouseX,
@@ -106,8 +140,4 @@ function mouseDragged() {
 	}
 	
 	//socket.emit('mouse', data);
-	
-	noStroke();
-	fill(0, 255, 100);
-	ellipse(data.x, data.y, 10, 10);
 }
