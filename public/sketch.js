7b7098b550b4b37147f1a9dcca3d79a724d4ab29
OptiGE <optige@gmail.com>
2019-06-21 23:02:21 +0200

    Städat upp kod och delat in i funktioner och objekt

--- public/sketch.js
+++ public/sketch.js
@@ -84,8 +84,8 @@ function loginScene(){
 	image(bg, 0, 0, windowWidth, windowHeight);
 	
 	//Skapa Create room knapp
-	btn_login = createSprite(0, 0, 600, 200);
-	addStdButton(btn_login, -0.4, 'assets/btn_login_up.png', 'assets/btn_login_p.png');
+	btn_login = newElement('stdButton', 0, 600, 200, ['assets/btn_login_up.png', 'assets/btn_login_p.png']);
+	
 	
 	//Knapp-event hanterare
 	btn_login.onMousePressed = function() {
@@ -110,13 +110,10 @@ function roomScene(name){
 	fill(30, 30, 30); // Svarta hela streck
 	text(name, windowWidth / 2, windowHeight / 3.5); //Kom ihåg att texten är center aligned
 	
-	//Skapa Join room knapp
-	btn_joinroom = createSprite(0, 0, 600, 200);
-	addStdButton(btn_joinroom, 0, 'assets/btn_joinroom_up.png', 'assets/btn_joinroom_p.png');
+	//Skapa knappar
+	btn_joinroom = newElement('stdButton', 0, 600, 200, ['assets/btn_joinroom_up.png', 'assets/btn_joinroom_p.png']);
+	btn_createroom = newElement('stdButton', 1, 600, 200, ['assets/btn_createroom_up.png', 'assets/btn_createroom_p.png']);
 	
-	//Skapa Create room knapp
-	btn_createroom = createSprite(0, 0, 600, 200);
-	addStdButton(btn_createroom, 1, 'assets/btn_createroom_up.png', 'assets/btn_createroom_p.png');
 	
 	//Knapp-event hanterare
 	btn_joinroom.onMousePressed = function() {
@@ -141,8 +138,8 @@ function roomScene(name){
 
 
 function gameScene(roomName, p1_nick){
+	
 	current_scene = 'game_scene';
-	console.log(roomName + " " + p1_nick);
 	
 	//Bakgrund
 	image(bg, 0, 0, windowWidth, windowHeight);
@@ -159,100 +156,34 @@ function gameScene(roomName, p1_nick){
 	textAlign(LEFT);
 	text(p1_nick, windowHeight / 25, windowHeight / 10);
 
-	
-	
+
 	//Skapa action-knappar
-	
-	btn_stop = createSprite(0, 0, 200, 200);
-	addActionButton(btn_stop, -18, 'assets/btn_stop_up.png', 'assets/btn_stop_p.png');
-	
- 	btn_left = createSprite(0, 0, 200, 200);
-	addActionButton(btn_left, -6, 'assets/btn_left_up.png', 'assets/btn_left_p.png');
-	
-	btn_right = createSprite(0, 0, 200, 200);
-	addActionButton(btn_right, 6, 'assets/btn_right_up.png', 'assets/btn_right_p.png');
-	
-	btn_fire = createSprite(0, 0, 200, 200);
-	addActionButton(btn_fire, 18, 'assets/btn_fire_up.png', 'assets/btn_fire_p.png'); 
+	btn_stop = newElement('actionButton', -18, 200, 200, ['assets/btn_stop_up.png', 'assets/btn_stop_p.png']);
+	btn_left = newElement('actionButton', -6, 200, 200, ['assets/btn_left_up.png', 'assets/btn_left_p.png']);
+	btn_right = newElement('actionButton', 6, 200, 200, ['assets/btn_right_up.png', 'assets/btn_right_p.png']);
+	btn_fire = newElement('actionButton', 18, 200, 200, ['assets/btn_fire_up.png', 'assets/btn_fire_p.png']);
 	
 	//Skapa actionfield
-	actionfield = createSprite(0, 0, 1000, 300);
-	placeActionfield(actionfield, 'assets/actionfield.png'); 
+	actionfield = newElement('actionField', 0, 1000, 300, ['assets/actionfield.png']);
 	
 	//Skapa actionslots
-	slot_1 = createSprite(0, 0, 200, 200);
-	addActionSlot(slot_1, -1, 'assets/btn_null.png', 'assets/btn_stop_up.png', 'assets/btn_left_up.png', 'assets/btn_right_up.png', 'assets/btn_fire_up.png');
+	slot_1 = newElement('actionSlot', -1, 200, 200, ['assets/btn_null.png', 'assets/btn_stop_up.png', 'assets/btn_left_up.png', 'assets/btn_right_up.png', 'assets/btn_fire_up.png']);
+	slot_2 = newElement('actionSlot', 0, 200, 200, ['assets/btn_null.png', 'assets/btn_stop_up.png', 'assets/btn_left_up.png', 'assets/btn_right_up.png', 'assets/btn_fire_up.png']);
+	slot_3 = newElement('actionSlot', 1, 200, 200, ['assets/btn_null.png', 'assets/btn_stop_up.png', 'assets/btn_left_up.png', 'assets/btn_right_up.png', 'assets/btn_fire_up.png']);
 	
-	slot_2 = createSprite(0, 0, 200, 200);
-	addActionSlot(slot_2, 0, 'assets/btn_null.png', 'assets/btn_stop_up.png', 'assets/btn_left_up.png', 'assets/btn_right_up.png', 'assets/btn_fire_up.png');
-	
-	slot_3 = createSprite(0, 0, 200, 200);
-	addActionSlot(slot_3, 1, 'assets/btn_null.png', 'assets/btn_stop_up.png', 'assets/btn_left_up.png', 'assets/btn_right_up.png', 'assets/btn_fire_up.png');
-	
+	//Actionfield-variabler
 	slotArray = [slot_1, slot_2, slot_3];
 	actionArray = [0, 0, 0];
 	
 	//Knapp-event hanterare
-	btn_stop.onMousePressed = function() {
-		if(buttons_clickable){
-			btn_stop.animation.changeFrame(1);
-			buttons_clickable = false;
-			if (actionArray.includes(0)) {
-				actionChosen(1);
-			}
-		}
-	};
-	btn_left.onMousePressed = function() {
-		if(buttons_clickable){
-			btn_left.animation.changeFrame(1);
-			buttons_clickable = false;
-			if (actionArray.includes(0)) {
-				actionChosen(2);
-			}
-		}
-	};
-	btn_right.onMousePressed = function() {
-		if(buttons_clickable){
-			btn_right.animation.changeFrame(1);
-			buttons_clickable = false;
-			if (actionArray.includes(0)) {
-				actionChosen(3);
-			}
-		}
-	};
-	btn_fire.onMousePressed = function() {
-		if(buttons_clickable){
-			btn_fire.animation.changeFrame(1);
-			buttons_clickable = false;
-			if (actionArray[actionArray.findIndex(k => k == 0) + 1] > 1 || actionArray.includes(4)) {
-				alert('bajs');
-			} else if (actionArray.includes(0)) {
-				actionChosen(4);
-				actionChosen(1);
-			}
-		}
-	};
-	slot_1.onMousePressed = function() {
-		if (buttons_clickable) {
-			buttons_clickable = false;
-			slot_1.animation.changeFrame(0);
-			actionArray[0] = 0;
-		}
-	};
-	slot_2.onMousePressed = function() {
-		if (buttons_clickable) {
-			buttons_clickable = false;
-			slot_2.animation.changeFrame(0);
-			actionArray[1] = 0;
-		}
-	};
-	slot_3.onMousePressed = function() {
-		if (buttons_clickable) {
-			buttons_clickable = false;
-			slot_3.animation.changeFrame(0);
-			actionArray[2] = 0;
-		}
-	};
+	btn_stop.onMousePressed = eventHandler.createActionHandler(1, btn_stop);
+	btn_left.onMousePressed = eventHandler.createActionHandler(2, btn_left);
+	btn_right.onMousePressed = eventHandler.createActionHandler(3, btn_right);
+	btn_fire.onMousePressed = eventHandler.createFireActionHandler(); //Denna är unik, så inga argument behövs
+
+	slot_1.onMousePressed = eventHandler.createSlotHandler(0, slot_1);
+	slot_2.onMousePressed = eventHandler.createSlotHandler(1, slot_2);
+	slot_3.onMousePressed = eventHandler.createSlotHandler(2, slot_3);
 
 }
 
@@ -300,52 +231,82 @@ function windowResized() {
 	console.log("Window resized");
 }
 
-function addStdButton(btn, heightOffset, img1, img2){
-	btn.addAnimation('standard', img1, img2);
-	btn.animation.playing = false;
-	btn.scale = windowHeight / (7*200); //scale sätts så att knappens höjd om 200 översätts till 1/7 av skärmen
-	btn.mouseActive = true;
-	btn.position.x = ((windowWidth) / 2);
-	btn.position.y = ((windowHeight + 100) / 2) + ((heightOffset * 200*btn.scale)*1.3);
-	console.log(btn.height);
+
+function newElement(type, offset, width, height, imageArray){
+	elem = createSprite(0, 0, width, height);
+	elem.addAnimation('standard', ...imageArray);
+	elem.animation.playing = false;
+	elem.mouseActive = true;		
+	switch(type) {
+		case 'stdButton':
+			elem.scale = windowHeight / (7*200); //scale sätts så att knappens höjd om 200 översätts till 1/7 av skärmen
+			elem.position.x = windowWidth / 2;
+			elem.position.y = ((windowHeight + 100) / 2) + ((offset * 200*elem.scale)*1.3);
+			break;
+		case 'actionField':
+			elem.scale = windowHeight / (7*300);
+			elem.position.x = ((windowWidth) / 2);
+			elem.position.y = windowHeight - (windowHeight - btn_stop.position.y) - elem.scale*300*1.3;
+			break;
+		case 'actionButton':
+			elem.scale = (windowHeight > windowWidth) ? windowWidth / (5*200) : windowWidth / (7*200);
+			elem.position.x = (windowWidth / 2) + offset * windowWidth / 50;
+			elem.position.y = (windowHeight > windowWidth) ? (windowHeight - (windowWidth*0.14)) : windowHeight*0.88;
+			break;
+		case 'actionSlot':
+			elem.scale = actionfield.scale;
+			elem.position.x = (windowWidth / 2) + offset * 1000 * actionfield.scale / 3;
+			elem.position.y = actionfield.position.y;
+			break;
+		default:
+			elem.position.x = windowWidth / 2;
+			elem.position.y = windowHeight / 2;
+	}
+	return elem;
 }
 
-function placeActionfield(btn, img1){
-	btn.addAnimation('standard', img1); //Single frame
-	btn.animation.playing = false;
-	btn.scale = windowHeight / (7*300); //scale sätts så att knappens höjd om 200 översätts till 1/7 av skärmen
-	btn.mouseActive = true;
-	btn.position.x = ((windowWidth) / 2);
-	btn.position.y = windowHeight - (windowHeight - btn_stop.position.y) - btn.scale*300*1.3;
-	console.log(btn.height);
+var eventHandler = {
+
+	createActionHandler(action, btn) {
+		return function() {
+			if(buttons_clickable){
+				btn.animation.changeFrame(1);
+				buttons_clickable = false;
+				if (actionArray.includes(0)) {
+					actionChosen(action);
+				}
+			}
+		}
+	},
+
+	createFireActionHandler() {
+		return function(){
+				if(buttons_clickable){
+					btn_fire.animation.changeFrame(1);
+					buttons_clickable = false;
+					if (actionArray[actionArray.findIndex(k => k == 0) + 1] > 1 || actionArray.includes(4)) {
+						alert('Forbidden move');
+					} else if (actionArray.includes(0)) {
+						actionChosen(4);
+						actionChosen(1);
+					}
+				}
+			   }
+	},
+
+	createSlotHandler(i, slot){
+		return function(){
+				  if (buttons_clickable) {
+					buttons_clickable = false;
+					slot.animation.changeFrame(0);
+					actionArray[i] = 0;
+				  }
+			   }
+	}
+
 }
 
-function addActionButton(btn, btnPosition, img1, img2){ //Samma som addStdButton, fast med position i sidled istället
-	btn.addAnimation('standard', img1, img2);
-	btn.animation.playing = false;
-	if (windowHeight > windowWidth){
-		btn.scale = windowWidth / (5*200); //scale sätts så att knappens bredd om 200 översätts till 1/5 av skärmen
-		btn.position.x = (windowWidth / 2) + btnPosition * windowWidth / 50;
-		btn.position.y = windowHeight - (windowWidth*0.14);
-	} else {
-		btn.scale = windowHeight / (7*200);
-		btn.position.x = (windowWidth / 2) + btnPosition * windowWidth / 50;
-		btn.position.y = windowHeight*0.88;
-	}		
 
-	btn.mouseActive = true;
-	console.log(btn.height);
-}
-
-function addActionSlot (slot, slotPosition, img0, img1, img2, img3, img4){
-	slot.addAnimation('standard', img0, img1, img2, img3, img4);
-	slot.animation.playing = false;
-	slot.scale = actionfield.scale; //scale sätts så att knappens bredd om 200 översätts till 1/5 av skärmen
-	slot.position.x = (windowWidth / 2) + slotPosition * 1000*actionfield.scale / 3;
-	slot.position.y = actionfield.position.y;
-	slot.mouseActive = true;
-	console.log(slot.height);
-}
 
 function actionChosen(frameNr) { 
 	actionArray[actionArray.findIndex(k => k == 0)] = frameNr;
